bae6d3e13a8b9e696b6ea853c03697b34b199330
Annie Ha <annieha@Annies-MacBook-Pro.local>
2021-09-22 13:43:19 +1000

    Trying to fix the Status Value

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 const taskName = document.querySelector("#taskName");
 const description = document.querySelector("#description");
 const assignTo = document.querySelector("#assignTo");
-
-
-let save = document.querySelector("#save");
-console.log(`here is the due date: ${dueDate.value}` );
-
-function validFormFieldInput(event){
+const dueDate = document.querySelector("#dueDate");
 const select = document.getElementById("status");
 var valueStatus = select.options[select.selectedIndex].value;
-let dueDate = document.querySelector("#dueDate");
+// const valueStatus = document.querySelector('#status');
+const save = document.querySelector("#save");
+
+console.log(`here is the due date: ${dueDate.value}` );
+console.log(`here is the value status: ${valueStatus.value}` );
+
+
+function validFormFieldInput(event){
+
 //validate Task Name field
     event.preventDefault();
+    // event.stopPropagation();
+
     if(taskName.value.length<=5){
         taskName.classList.remove('is-valid');
         taskName.classList.add('is-invalid');}
@@ -19,6 +24,7 @@ let dueDate = document.querySelector("#dueDate");
         taskName.classList.add('is-valid');
         taskName.classList.remove('is-invalid');
     }
+
 // validate description field
     if(description.value.length<=5){
         description.classList.remove('is-valid');
@@ -37,22 +43,24 @@ let dueDate = document.querySelector("#dueDate");
         assignTo.classList.remove('is-invalid');
     }
     
-    // // validate status field
-    if(valueStatus===""){
-        valueStatus.classList.remove('is-valid');
-        valueStatus.classList.add('is-invalid');}
-    else{
-        valueStatus.classList.add('is-valid');
-        valueStatus.classList.remove('is-invalid');
-    }
-    
-    // // validate Due Date field
+    // validate Due Date field
     if(dueDate.value===""){
         dueDate.classList.remove('is-valid');
         dueDate.classList.add('is-invalid');}
     else{
         dueDate.classList.add('is-valid');
         dueDate.classList.remove('is-invalid');
+    }
+
+    // validate status field
+    if(valueStatus===""){
+        console.log('Status is not selected yet');
+        valueStatus.classList.remove('is-valid');
+        valueStatus.classList.add('is-invalid');}
+    else{
+        console.log('Status is selected yet');
+        valueStatus.classList.add('is-valid');
+        valueStatus.classList.remove('is-invalid');
     }
     
 }
