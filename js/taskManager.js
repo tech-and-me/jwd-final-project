dee28e8fda6a6aea282cb1319f6375ce44b67f6f
Pary <81phary@gmail.com>
2021-09-23 09:28:05 +0700

    adding codes for task 6

--- js/taskManager.js
+++ js/taskManager.js
@@ -1,5 +1,19 @@
-class taskManager{
+class TaskManager{
     constructor(currentID = 0){
         this.tasks = [];
+        this.currentID = currentID;
+    }
+
+    addTask(name,description,assignedTo,dueDate,status){
+        const newTask ={
+            id: this.currentID++,
+            name:name,
+            description:description,
+            assignedTo:assignedTo,
+            dueDate:dueDate,
+            status:status
+        }
+        this.tasks.push(newTask);
+
     }
 }
